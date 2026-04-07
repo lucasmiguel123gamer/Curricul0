2601d37979129f7af7f40e57c05365f38c1a8125
lucasmiguel123gamer <lucas.gines@escola.pr.gov.br>
2026-04-07 11:32:57 -0300

    Projeto versâo 1

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import fotoPerfil from './WhatsApp Image 2026-03-24 at 12.14.28.jpeg'
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1></h1>
+        <img src={fotoPerfil}></img>
+        <h2>Lucas Gabriel=</h2>
+      </header>
+
+      <main>
+        <section>
+          <h1>Dados para contato</h1>
+          <p>Lucas gabriel</p>
+          <p>44 99881-2272</p>
+          <p>Email: lucas.gines@escola.pr.gov.br</p>
+          <p>Linkdin: xxxxxxx</p>
+          <p>Github: xxxx</p>
+        </section>
+        <section>
+          <h1>Formação</h1>
+          <div>
+            <article>
+              <h2>Ensino Medio Profissionalizante em Desenvolvimento de Sistemas</h2>
+              <p>Instituição:Colegio Santa Maria Goretti</p>
+              <p>Ano de Conclusão: 2027</p>
+            </article>
+
+            <article>
+              <h2></h2>
+              <p>Instituição</p>
+              <p>Ano de Conclusão:</p>
+            </article>
+
+          </div>
+        </section>
+        <section>
+
+        </section>
+      </main>
+
+      </div>
+      );
+}
+      export default App;
